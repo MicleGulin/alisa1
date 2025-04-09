f5b67e0d61de540991ca0661ae9db7e62a8fe485
admin <m-gulin@ya.ru>
2025-04-09 23:49:21 +0300

    Исправлен другой чужой навык

--- server.py
+++ server.py
@@ -1,9 +1,7 @@
-# импортируем библиотеки
 from flask import Flask, request, jsonify
-import logging
+import logging, os
 
 app = Flask(__name__)
-
 logging.basicConfig(level=logging.INFO)
 sessionStorage = {}
 
@@ -11,77 +9,56 @@ sessionStorage = {}
 @app.route('/post', methods=['POST'])
 def main():
     logging.info(f'Request: {request.json!r}')
-
     response = {
         'session': request.json['session'],
         'version': request.json['version'],
-        'response': {
-            'end_session': False
-        }
+        'response': {'end_session': False}
     }
 
-    handle_dialog(request.json, response)
-
+    processing_dialog(request.json, response)
     logging.info(f'Response:  {response!r}')
-
-    # Преобразовываем в JSON и возвращаем
     return jsonify(response)
 
 
-def handle_dialog(req, res):
+def processing_dialog(req, res):
     user_id = req['session']['user_id']
 
     if req['session']['new']:
-
-        sessionStorage[user_id] = {
-            'suggests': [
-                "Не хочу.",
-                "Не буду.",
-                "Отстань!",
-            ]
-        }
+        sessionStorage[user_id] = {'suggests': ["Не хочу.", "Не буду.", "Отстань!"], 'current': 'слон'}
         res['response']['text'] = 'Привет! Купи слона!'
         res['response']['buttons'] = get_suggests(user_id)
-        return
-
-
-    if req['request']['original_utterance'].lower() in [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо',
-        'я покупаю',
-        'я куплю'
-    ]:
-        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
-        res['response']['end_session'] = True
-        return
-
-    res['response']['text'] = \
-        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
-    res['response']['buttons'] = get_suggests(user_id)
+    else:
+        a = [i for i in req['request']['nlu']['tokens'] if i in ['ладно', 'куплю', 'покупаю', 'хорошо']]
+        if a:
+            if sessionStorage[user_id]['current'] == 'слон':
+                res['response']['text'] = 'Слона можно найти на Яндекс.Маркете! Купи еще кролика!'
+                sessionStorage[user_id]['current'] = 'кролик'
+                sessionStorage[user_id]['suggests'] = ["Не хочу.", "Не буду.", "Отстань!"]
+            else:
+                res['response']['text'] = 'Кролика можно найти на Яндекс.Маркете! Купи еще слона!'
+                sessionStorage[user_id]['current'] = 'слон'
+                sessionStorage[user_id]['suggests'] = ["Не хочу.", "Не буду.", "Отстань!"]
+            res['response']['buttons'] = get_suggests(user_id)
+        else:
+            res['response'][
+                'text'] = f"Все говорят '{req['request']['original_utterance']}', а ты купи {sessionStorage[user_id]['current']}а!"
+            res['response']['buttons'] = get_suggests(user_id)
 
 
 def get_suggests(user_id):
     session = sessionStorage[user_id]
-
-    suggests = [
-        {'title': suggest, 'hide': True}
-        for suggest in session['suggests'][:2]
-    ]
-
+    suggests = [{'title': suggest, 'hide': True} for suggest in session['suggests'][:2]]
     session['suggests'] = session['suggests'][1:]
     sessionStorage[user_id] = session
-
     if len(suggests) < 2:
         suggests.append({
             "title": "Ладно",
-            "url": "https://market.yandex.ru/search?text=слон",
+            "url": f"https://market.yandex.ru/search?text={sessionStorage[user_id]['current']}",
             "hide": True
         })
-
     return suggests
 
 
 if __name__ == '__main__':
-    app.run()
+    port = os.environ.get('PORT', 4545)
+    app.run(host='0.0.0.0', port=port)
